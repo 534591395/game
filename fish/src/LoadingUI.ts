4f457788c00c6843e78deadac383330c30c4aad5
hzwangyang1 <hzwangyang1@corp.netease.com>
2019-01-24 16:22:34 +0800

    【MOD】loading皮肤

--- fish/src/LoadingUI.ts
+++ fish/src/LoadingUI.ts
@@ -27,25 +27,28 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////
 
-class LoadingUI extends egret.Sprite implements RES.PromiseTaskReporter {
+class LoadingUI extends eui.Component implements RES.PromiseTaskReporter {
+    private loading: eui.Image;
 
     public constructor() {
         super();
         this.createView();
+        this.skinName = "resource/skins/loading.exml";
     }
 
     private textField: egret.TextField;
 
     private createView(): void {
-        this.textField = new egret.TextField();
-        this.addChild(this.textField);
-        this.textField.y = 300;
-        this.textField.width = 480;
-        this.textField.height = 100;
-        this.textField.textAlign = "center";
+        // this.textField = new egret.TextField();
+        // this.addChild(this.textField);
+        // this.textField.y = 300;
+        // this.textField.width = 480;
+        // this.textField.height = 100;
+        // this.textField.textAlign = "center";
     }
 
     public onProgress(current: number, total: number): void {
-        this.textField.text = `Loading...${current}/${total}`;
+        //this.textField.text = `Loading...${current}/${total}`;
+        this.loading.width = (current / total) * 428;
     }
 }
--- fish/src/Main.ts
+++ fish/src/Main.ts
@@ -94,10 +94,11 @@ class Main extends eui.UILayer {
 
     private async loadResource() {
         try {
-            const loadingView = new LoadingUI();
-            this.stage.addChild(loadingView);
             await RES.loadConfig("resource/default.res.json", "resource/");
             await this.loadTheme();
+            await RES.loadGroup("loading", 1);
+            const loadingView = new LoadingUI();
+            this.stage.addChild(loadingView);
             await RES.loadGroup("preload", 0, loadingView);
             this.stage.removeChild(loadingView);
         }
