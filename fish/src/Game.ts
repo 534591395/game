de127601398544b2916145046dc46488c0e7e4cf
hzwangyang1 <hzwangyang1@corp.netease.com>
2019-01-25 18:11:10 +0800

    【MOD】新增 Game类，游戏主逻辑

---
+++ fish/src/Game.ts
@@ -0,0 +1,226 @@
+/**
+ * 游戏主逻辑
+ */
+
+class Game extends egret.DisplayObjectContainer {
+    // 计时器
+    private timer:egret.Timer;
+    // 倒计时频率（默认1s）
+    private time:number = 1000;
+    // 当前关卡的通关时间（默认20s）
+    public timeNum:number = 20;
+    // 当前关卡剩余倒计时
+    private timeTxtNum:number = 0;
+    // 倒计时显示的文案控件
+    private txt:egret.TextField;
+    // 控制生成泡泡鱼个数（通过除以一个整数），当前帧率变动时，不断累加
+    private counter:number = 0;
+    // 生成界面上泡泡鱼个数频率
+    private frequencyFish:number = 10;
+    // 被用户点破的泡泡鱼数量（分数）
+    private delFish:number = 0;
+    // 回收站里的泡泡鱼
+    private spareFishes:Fish[] = [];
+    // 已生成的泡泡鱼实例（new Fish）, 通过 fish.enabled 属性确定该泡泡鱼是否在界面上
+    private fishes:Fish[] = [];
+    // 泡泡鱼爆炸后的粒子发射器（粒子效果）
+    private fishEmitter:FishEmitter;
+    // UI面板
+    private scorePanel:ScorePanel;
+
+    public constructor() {
+        super();
+        this.render();
+    }
+
+    private render() {
+        this.fishEmitter = new FishEmitter();
+        this.addChild(this.fishEmitter);
+        
+        this.pannelUI();
+        this.gameTimer();
+        this.TxtTimer();
+    }
+    
+    // 操作以及提示面板
+    private pannelUI() {
+        this.scorePanel = new ScorePanel();
+        this.scorePanel.touchEnabled = true;
+        this.scorePanel.x = 0;
+        this.scorePanel.y = 100;
+        this.addChild(this.scorePanel);
+        // 显示面板
+        this.scorePanel.showStart();
+        // 监听触发游戏开始
+        this.scorePanel.addEventListener(CustomHandleEvent.GameStart, this.gameStart, this);
+    }
+
+    // 游戏计时器（倒计时）
+    private gameTimer() {
+        this.timer = new egret.Timer(this.time, this.timeNum);
+        //注册事件侦听器
+        this.timer.addEventListener(egret.TimerEvent.TIMER,this.timerFunc,this);
+        this.timer.addEventListener(egret.TimerEvent.TIMER_COMPLETE,this.timerComFunc,this);
+    }
+
+    // 倒计时文案提示组件（显示在界面左上角）
+    private TxtTimer() {
+        this.txt = new egret.TextField();
+        this.txt.width = 70;
+        this.txt.height = 50;
+        this.txt.textAlign = "center";
+        this.txt.textColor = 0xFFFFFF;
+        this.txt.size = 30;
+        this.txt.y = 20;
+        this.txt.x = 20;
+        this.addChild(this.txt);
+    }
+
+    // 计时器回调，设置当前倒计时
+    private timerFunc() {
+        this.txt.text = --this.timeTxtNum + 'S';
+    }
+
+    // 计时器回调，倒计时完成后（当前关卡已到规定时间）
+    private timerComFunc() {
+        this.gameEnd();
+    }
+
+    // 当前游戏关卡结束
+    private gameEnd() {
+        // 关卡已结束，移除监听帧率
+        this.removeEventListener(egret.Event.ENTER_FRAME, this.onGameLoop, this);
+        // 显示得分
+        this.scorePanel.showScore(this.delFish);
+        // 添加操作界面UI
+        this.addChild(this.scorePanel);
+    }
+    
+    // 开始当前游戏关卡
+    private gameStart() {
+        // 重新设置当前关卡剩余倒计时
+        this.timeTxtNum = this.timeNum;
+        // 设置倒计时显示组件的文案
+        this.txt.text = this.timeTxtNum + 'S';
+        // 计时器重置
+        this.timer.reset();
+        // 开始关卡倒计时
+        this.timer.start();
+        // 游戏开始后，移除操作界面UI
+        this.removeChild(this.scorePanel);
+        // 开始监听帧率，不断在界面上生成泡泡鱼
+        this.addEventListener(egret.Event.ENTER_FRAME, this.onGameLoop, this);
+    }
+
+    // 帧率回调：执行onGameLoop
+    private onGameLoop() {
+        // 每隔 frequencyFish数，界面上显示一个泡泡鱼
+        if(this.counter++ % this.frequencyFish == 0) {
+            this.makeFish();
+        }
+        // 每个泡泡鱼更新属性（坐标，缩放，移除）
+        this.fishes.map( fish => {
+            // 只有在界面上的泡泡鱼才更新属性
+            if (fish.enabled) {
+               // 更新属性坐标，（旋转效果未实现，z轴问题）
+               fish.update();
+               // 更新属性后，设置变动的属性，表现为泡泡鱼在做运动（移动，缩放）
+               fish.render();
+
+               // 当泡泡鱼往上移动到屏幕外面时，移除屏幕
+               if (fish.posY < -200) {
+                   this.removeFish(fish);
+               }
+            }
+        });
+        // 实时更新粒子
+        this.fishEmitter.update();
+    }
+
+    // 创造泡泡鱼
+    private makeFish() {
+        let fish:Fish;
+        // 先使用回收站里的泡泡鱼实例
+        if (this.spareFishes.length) {
+            // 从最后一个取
+            fish = this.spareFishes.pop();
+            fish.enabled = true;
+            this.addChild(fish);
+        } else {
+            // 泡泡鱼（纹理）种类：orangefish01 ,..., orangefish04
+            let fishBitmap:egret.Bitmap = this.createBitmapByName('orangefish0'+ ( (this.fishes.length%4) + 1 )+'_png');
+            // 实例一个泡泡鱼
+            fish = new Fish(0,0,0, fishBitmap);
+            // 加入
+            this.fishes.push(fish);
+            // 设置可触摸（触摸后爆炸效果）
+            fish.touchEnabled = true;
+            // 监听触摸事件，触发爆炸效果
+            fish.addEventListener( egret.TouchEvent.TOUCH_TAP, this.explodeFish, this );
+            fish.addEventListener( egret.TouchEvent.TOUCH_MOVE, this.explodeFish, this );
+            // 加入界面
+            this.addChild(fish);
+        }
+
+        // 设置泡泡鱼初始属性
+        const HALFWIDTH = this.stage.stageWidth/2;
+        fish.posX = HALFWIDTH + this.randomRange(-250,250);
+        fish.posY = this.stage.stageHeight + 10;
+        fish.posZ = this.randomRange(-250,250);
+        
+        // 更新位置频率（累加值）
+        fish.velX = this.randomRange(-1,1);
+        fish.velY = this.randomRange(-1,-2);
+        fish.velZ = this.randomRange(-1,1);
+        // 缩放比
+        fish.size = 1;
+        // 重力加速度
+        fish.gravity = -0.3;
+    }
+
+    // 泡泡鱼爆炸（粒子效果）
+    private explodeFish(event: egret.Event) {
+        const fish:Fish = event.target;
+        // 根据爆炸位置，创建n个小粒子，实现爆炸效果（粒子发送器）
+        this.fishEmitter.makeExplosion(fish.posX, fish.posY);
+        this.removeFish(fish);
+        // 累加戳破泡泡鱼
+        this.delFish ++;
+    }
+    
+    //移除界面上的泡泡鱼
+    private removeFish(fish: Fish) : any {
+        // 设置属性（不可用）后，每次帧率变动时候，不再更新该 fish 属性
+        fish.enabled = false;
+        // 界面上移除
+        this.removeChild(fish);
+        // 放入回收站
+        this.spareFishes.push(fish);
+    }
+
+    // 关卡游戏结束后，清除所有界面上的泡泡鱼
+    private clearFish() : any {
+        // 戳破的泡泡鱼计数清零
+        this.delFish = 0;
+        // 累加器清零
+        this.counter = 0;
+        this.fishes.map( fish => {
+            if (fish.enabled) {
+                this.removeFish(fish);
+            }
+        });
+    }
+    
+    //随机数
+    private randomRange(min:number, max:number) : number {
+        return (Math.random()*(max-min))+min;
+    }
+    
+    // 取指定纹理
+    private createBitmapByName(name: string): egret.Bitmap {
+        let result = new egret.Bitmap();
+        let texture: egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    }
+}
--- fish/src/Main.ts
+++ fish/src/Main.ts
@@ -29,36 +29,9 @@
 
 class Main extends eui.UILayer {
 
-    // 计时器
-    private timer:egret.Timer;
-
-    private counter:number = 0;
-
-    private delFish:number = 0;
-
     private bg:BgMap;
 
-    private button:eui.Button;
-
-    private shp:Btn;
-
-    private time:number = 1000;
-
-    private timeNum:number = 20;
-
-    private timeTxtNum:number = 0;
-
-    private txt:egret.TextField;
-
-    // 回收站的鱼
-    private spareFishes = [];
-    // 运动中的鱼
-    private fishes = [];
-
-    private emitter: FishEmitter;
-
-    private scorePanel: ScorePanel;
-
+    private game:Game;
 
     protected createChildren(): void {
         super.createChildren();
@@ -129,164 +102,12 @@ class Main extends eui.UILayer {
     protected createGameScene(): void {
         this.bg = new BgMap();
         this.addChild(this.bg);
-        this.emitter = new FishEmitter();
-        this.addChild(this.emitter);
-        this.scorePanel = new ScorePanel();
-        this.scorePanel.touchEnabled = true;
-        this.scorePanel.x = 0;
-        this.scorePanel.y = 100;
-        this.scorePanel.showStart();
-        this.scorePanel.addEventListener(CustomHandleEvent.GameStart, this.gameStart, this);
-        this.addChild(this.scorePanel);
-        this.timerS();
-        this.timerTxt();
         // 初始化开始按钮
         this.bg.start();
         
-    }
-    // 定时器--倒计时文案
-    private timerTxt() {
-        this.txt = new egret.TextField();
-        this.txt.width = 70;
-        this.txt.height = 50;
-        this.txt.textAlign = "center";
-        this.txt.textColor = 0xFFFFFF;
-        this.txt.size = 30;
-        this.txt.y = 20;
-        this.txt.x = 20;
-        this.addChild(this.txt);
-    }
-    
-    /**
-     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
-     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
-     */
-    private createBitmapByName(name: string): egret.Bitmap {
-        let result = new egret.Bitmap();
-        let texture: egret.Texture = RES.getRes(name);
-        result.texture = texture;
-        return result;
-    }
-
-    // 计时器
-    private timerS() {
-        this.timer = new egret.Timer(this.time, this.timeNum);
-        //注册事件侦听器
-        this.timer.addEventListener(egret.TimerEvent.TIMER,this.timerFunc,this);
-        this.timer.addEventListener(egret.TimerEvent.TIMER_COMPLETE,this.timerComFunc,this);
-    }
-
-    // 计时
-    private timerFunc() {
-        this.txt.text = --this.timeTxtNum + 'S';
-        console.log(egret.getTimer());
-    }
-
-    // 计时结束
-    private timerComFunc() {
-        this.gameEnd();
-    }
-
-    private gameStart() {
-        this.clearFish();
-        this.timeTxtNum = this.timeNum;
-        this.txt.text = this.timeTxtNum + 'S';
-        this.timer.reset();
-        this.timer.start();
-        try {
-            this.removeChild(this.scorePanel);
-        } catch (error) {
-            
-        }
+        this.game = new Game();
+        this.addChild(this.game);
         
-        this.addEventListener(egret.Event.ENTER_FRAME, this.onGameLoop, this);
-    }
-
-    private gameEnd() {
-        this.removeEventListener(egret.Event.ENTER_FRAME, this.onGameLoop, this);
-        this.scorePanel.showScore(this.delFish);
-        this.addChild(this.scorePanel);
-    }    
-
-    private onGameLoop() {
-        if(this.counter++ % 10 == 0) {
-            this.makeFish();
-        }
-        for( let i=0; i<this.fishes.length; i++ ){
-            const fish = this.fishes[i];
-            if(!fish.enabled) continue;
-
-            fish.update();
-
-            fish.render();
-
-            if(fish.posY <-200) {
-                this.removeFish(fish);
-            };
-        }
-        this.emitter.update();
-    }
-
-    private makeFish() {
-        let fish;
-        if (this.spareFishes.length) {
-            fish = this.spareFishes.pop();
-            fish.enabled = true;
-            this.addChild(fish);
-        } else {
-             let fishBitmap = this.createBitmapByName('orangefish0'+ ( (this.fishes.length%4) + 1 )+'_png');
-             fish = new Fish(0,0,0, fishBitmap); 
-             this.fishes.push(fish);
-             fish.touchEnabled = true;
-             fish.addEventListener( egret.TouchEvent.TOUCH_TAP, this.explodeFish, this );
-             fish.addEventListener( egret.TouchEvent.TOUCH_MOVE, this.explodeFish, this );
-
-             this.addChild(fish);
-        }
-        const HALFWIDTH = this.stage.stageWidth/2;
-        fish.posX = HALFWIDTH + this.randomRange(-250,250);
-        fish.posY = this.stage.stageHeight + 10;
-        fish.posZ = this.randomRange(-250,250);
-
-        fish.velX = this.randomRange(-1,1);
-        fish.velY = this.randomRange(-1,-2);
-        fish.velZ = this.randomRange(-1,1);
-
-        fish.size = 1;
-        fish.gravity = -0.3;
-    }
-
-    // 泡泡鱼爆炸效果
-    private explodeFish(event:egret.Event) {
-        const fish = event.target;
-        this.emitter.makeExplosion(fish.posX, fish.posY);
-        this.removeFish(fish);
-        this.delFish ++;
-    }
-
-    private removeFish(fish) {
-        fish.enabled = false;
-        this.removeChild(fish);
-        this.spareFishes.push(fish);
-    }
-
-    private clearFish() {
-        this.delFish = 0;
-        this.counter = 0;
-        this.spareFishes = [];
-        this.fishes.map((fish) => {
-            try {
-                this.removeChild(fish);
-            } catch (error) {
-                
-            }
-            
-        });
-        this.fishes = [];
-    }
-
-    private randomRange(min:number, max:number) {
-        return (Math.random()*(max-min))+min;
     }
 
 }
