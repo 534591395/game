4677f26f215ee4e0801f929ed23ab0762507fbeb
hzwangyang1 <hzwangyang1@corp.netease.com>
2019-11-29 14:17:50 +0800

    【】

--- fish/libs/modules/assetsmanager/assetsmanager.d.ts
+++ fish/libs/modules/assetsmanager/assetsmanager.d.ts
@@ -558,7 +558,7 @@ declare module RES {
      * @platform Web,Native
      * @language zh_CN
      */
-    function getResAsync(key: string): Promise<any> | void;
+    function getResAsync(key: string): Promise<any>;
     /**
      * Asynchronous mode to get the resources in the configuration. As long as the resources exist in the configuration file, you can get it in an asynchronous way.
      * @param key A sbuKeys attribute or name property in a configuration file.
@@ -1041,6 +1041,15 @@ declare module RES.processor {
      */
     function getRelativePath(url: string, file: string): string;
     var ImageProcessor: Processor;
+    const KTXTextureProcessor: RES.processor.Processor;
+    /**
+    *
+    */
+    function makeEtc1SeperatedAlphaResourceInfo(resource: ResourceInfo): ResourceInfo;
+    /**
+    *
+    */
+    const ETC1KTXProcessor: Processor;
     var BinaryProcessor: Processor;
     var TextProcessor: Processor;
     var JsonProcessor: Processor;
--- fish/libs/modules/game/game.js
+++ fish/libs/modules/game/game.js
@@ -2842,13 +2842,15 @@ var egret;
             function onLoadComplete(e) {
                 removeListeners();
                 var bitmapData = imageLoader.data;
-                bitmapData.source.setAttribute("bitmapSrc", virtualUrl);
+                if (bitmapData.source.setAttribute) {
+                    bitmapData.source.setAttribute("bitmapSrc", virtualUrl);
+                }
                 var texture = new egret.Texture();
                 texture._setBitmapData(bitmapData);
                 loader.data = texture;
                 window.setTimeout(function () {
                     loader.dispatchEventWith(egret.Event.COMPLETE);
-                }, self);
+                }, 0);
             }
             function removeListeners() {
                 imageLoader.removeEventListener(egret.Event.COMPLETE, onLoadComplete, self);
@@ -4396,7 +4398,7 @@ var egret;
  * @private
  */
 egret["testDeviceType1"] = function () {
-    if (!window["navigator"]) {
+    if (!window["navigator"] || !navigator) {
         return true;
     }
     var ua = navigator.userAgent.toLowerCase();
